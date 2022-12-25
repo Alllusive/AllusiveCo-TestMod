aa3dfaf128bfe6b43cf2cafc7850042ed5f39c01
Alllusive <allusivedxdco@gmail.com>
2022-12-25 14:16:57 +1100

    Initial commit

---
+++ src/main/java/net/allusive/allusiveco/AllusiveCo.java
@@ -0,0 +1,15 @@
+package net.allusive.allusiveco;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AllusiveCo implements ModInitializer {
+	public static final String MOD_ID = "allusiveco";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/allusive/allusiveco/AllusiveCoClient.java
@@ -0,0 +1,10 @@
+package net.allusive.allusiveco;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AllusiveCoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
